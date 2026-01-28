7e7200840234b4c539ff92602f2afed04a1bd70c
FredrikMWold <fredrik.wold@bouvet.no>
2026-01-28 17:18:06 +0100

    feat(git-editor): Adds a new extentsion that lets the user use vicinae as a git editor

---
+++ extensions/git-editor/src/hooks/useRebase.tsx
@@ -0,0 +1,121 @@
+import { closeMainWindow } from "@vicinae/api";
+import { readFile, writeFile } from "fs/promises";
+import { dirname } from "path";
+import { useEffect, useState } from "react";
+import { getCommitMetadata } from "../utils/getCommitMetadata";
+
+export const useRebase = (gitFile?: string) => {
+  const [commits, setCommits] = useState<Commit[]>([]);
+
+  useEffect(() => {
+    const fetchFileContent = async () => {
+      if (!gitFile) return;
+      const content = await readFile(gitFile, "utf-8");
+
+      let commits = content
+        .split("\n")
+        .filter((line) => !line.startsWith("#") && line);
+
+      const gitDir = dirname(gitFile);
+
+      const mappedCommits = commits
+        .map((line) => {
+          const [meta, message] = line.split(" # ");
+          const [type, hash] = meta.split(" ");
+          const metadata = getCommitMetadata(hash, gitDir);
+          return {
+            type: type as CommitType,
+            hash,
+            fullHash: metadata.fullHash,
+            message: message.trim(),
+            naturalDate: metadata.naturalDate,
+            date: metadata.date,
+            author: metadata.author,
+            branches: metadata.branches,
+            tags: metadata.tags,
+            fileStats: metadata.fileStats,
+          };
+        })
+        .reverse();
+
+      setCommits(mappedCommits);
+    };
+    fetchFileContent();
+  }, [gitFile]);
+
+  const startRebase = async () => {
+    if (!gitFile) return;
+    const content = commits
+      .toReversed()
+      .map((commit) => `${commit.type} ${commit.hash} # ${commit.message}`)
+      .join("\n");
+    writeFile(gitFile, content);
+    closeMainWindow();
+  };
+
+  const moveCommitDown = (commit: Commit) => {
+    setCommits((prev) => {
+      const currentIndex = prev.findIndex((c) => c.hash === commit.hash);
+      if (currentIndex >= prev.length - 1) return prev;
+      const tempCurrent = prev[currentIndex];
+      const tempBelow = prev[currentIndex + 1];
+      const newCommits = [...prev];
+      newCommits[currentIndex + 1] = tempCurrent;
+      newCommits[currentIndex] = tempBelow;
+      return newCommits;
+    });
+  };
+
+  const moveCommitUp = (commit: Commit) => {
+    setCommits((prev) => {
+      const currentIndex = prev.findIndex((c) => c.hash === commit.hash);
+      if (currentIndex <= 0) return prev;
+      const tempCurrent = prev[currentIndex];
+      const tempAbove = prev[currentIndex - 1];
+      const newCommits = [...prev];
+      newCommits[currentIndex - 1] = tempCurrent;
+      newCommits[currentIndex] = tempAbove;
+      return newCommits;
+    });
+  };
+
+  const updateCommit = (commit: Commit, type: CommitType) => {
+    setCommits(
+      commits.map((c) => (c.hash === commit.hash ? { ...c, type } : c)),
+    );
+  };
+
+  return {
+    commits,
+    setCommits,
+    startRebase,
+    moveCommitUp,
+    moveCommitDown,
+    updateCommit,
+  };
+};
+
+type Commit = {
+  message: string;
+  hash: string;
+  fullHash: string;
+  naturalDate: string;
+  date: string;
+  author: string;
+  branches: string[];
+  tags: string[];
+  fileStats: {
+    added: number;
+    modified: number;
+    removed: number;
+  };
+  type: CommitType;
+};
+
+export type CommitType =
+  | "pick"
+  | "reword"
+  | "edit"
+  | "squash"
+  | "fixup"
+  | "drop";
